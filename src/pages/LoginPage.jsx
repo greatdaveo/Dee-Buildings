2e4b1a2f64f8f56d6af57f23435302aa14fd252d
David Olowomeye <olowomeyedavid242@gmail.com>
2024-01-17 09:55:54 +0000

    Added redux toolkit for global state management

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,11 +1,17 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  loginStart,
+  loginSuccess,
+  loginFailure,
+} from "../redux/user/userSlice";
 
 const LoginPage = () => {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
@@ -14,7 +20,9 @@ const LoginPage = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
+      // When login state is initiated
+      dispatch(loginStart());
+
       const response = await fetch("/api/auth/login", {
         method: "POST",
         headers: {
@@ -23,20 +31,18 @@ const LoginPage = () => {
         body: JSON.stringify(formData),
       });
       const data = await response.json();
-
+      console.log(data)
+      // When the data fetched is false
       if (data.success === false) {
-        setError(data.message);
-        setLoading(false);
-
+        dispatch(loginFailure(data.message));
         return;
       }
-
-      setLoading(false);
-      setError(null);
+      // When it login is successful
+      dispatch(loginSuccess(data));
       navigate("/");
     } catch (error) {
-      setLoading(false);
-      setError(error.message);
+      // When there is an outright error
+      dispatch(loginFailure(error.message));
     }
   };
 
